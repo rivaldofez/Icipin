ff234eb576973f791093004c328dd33c9879fd6b
rivaldofez <rivaldofez@gmail.com>
2022-05-17 12:34:36 +0700

    create modal location view file

---
+++ McEat/ModalLocationView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalLocationView.swift
+//  McEat
+//
+//  Created by Rivaldo Fernandes on 17/05/22.
+//
+
+import SwiftUI
+
+struct ModalLocationView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct ModalLocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalLocationView()
+    }
+}

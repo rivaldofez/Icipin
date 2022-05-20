8b4d1a6f3f2fb2a3713f0115e906aff23b8fa718
rivaldofez <rivaldofez@gmail.com>
2022-05-20 22:13:19 +0700

    pass binding data from swiftUI to UIKit

--- McEat/ScanpageView.swift
+++ McEat/ScanpageView.swift
@@ -11,68 +11,88 @@ import Vision
 
 struct ScanpageView: View {
     @State var timeRemaining = 10
-        let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var isShow = false
+    @State var prediction = ""
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         //call UIKit VC
         
         ZStack{
-            MainVCView()
+//            MainVCView(isShow: self.$isShow)
             
-            Text("\(timeRemaining)")
-                .onReceive(timer) { _ in
-                    if timeRemaining > 0 {
-                        timeRemaining -= 1
-                }
-            }
+            ScanPageCustomView(prediction: self.$prediction)
+            Text(prediction)
+//            Text("\(timeRemaining)")
+//                .onReceive(timer) { _ in
+//                    if timeRemaining > 0 {
+//                        timeRemaining -= 1
+//                }
+//            }
+            
         }
     }
 }
 
-struct ScanpageView_Previews: PreviewProvider {
-    static var previews: some View {
-        ScanpageView()
-    }
+protocol CustomDelegate {
+    func didUpdateWithValue(_ value: String)
 }
 
-
-//create bridge SwiftUI and UIKit
-struct MainVCView: UIViewControllerRepresentable {
+struct ScanPageCustomView: UIViewControllerRepresentable {
+    @Binding var prediction: String
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let vc = ScanPageViewController()
+        vc.customDelegate = context.coordinator
+        return vc
+    }
+    
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
     }
     
-    func makeUIViewController(context: Context) -> some UIViewController {
-        //create view controller object
-        return ScanpageViewController()
+    func makeCoordinator() -> Coordinator {
+        Coordinator(customView: self)
+    }
+    
+    class Coordinator: NSObject, CustomDelegate {
+        var parent: ScanPageCustomView
+        
+        init(customView: ScanPageCustomView){
+            self.parent = customView
+        }
+        
+        func didUpdateWithValue(_ value: String) {
+            parent.prediction = value
+        }
     }
 }
 
 
-// create view controller
-class ScanpageViewController: UIViewController,  AVCaptureVideoDataOutputSampleBufferDelegate {
+class ScanPageViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    var customDelegate: CustomDelegate?
     private var requests = [VNRequest]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         //here to start the camera
         let captureSession = AVCaptureSession()
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {
             return
         }
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        
+
         captureSession.addInput(input)
-        
+
         captureSession.sessionPreset = .photo
         captureSession.sessionPreset = .vga640x480
         captureSession.startRunning()
-        
+
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
-        
-        
+
+
         let dataOutput = AVCaptureVideoDataOutput()
         if(captureSession.canAddOutput(dataOutput)){
             captureSession.addOutput(dataOutput)
@@ -80,9 +100,7 @@ class ScanpageViewController: UIViewController,  AVCaptureVideoDataOutputSampleB
             dataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
             dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem))
         }
-        
-        
-        
+
     }
     
     func setupVision(){
@@ -95,26 +113,33 @@ class ScanpageViewController: UIViewController,  AVCaptureVideoDataOutputSampleB
                 fatalError("Couldn't create detector")
             }
         }()
-        
+
         guard let vnModel = try? VNCoreMLModel(for: model.model) else {return}
-        
+
         self.requests = [VNCoreMLRequest(model: vnModel) { finishedReq, err in
             guard let results = finishedReq.results as? [VNRecognizedObjectObservation] else {return}
             guard let firstResult = results.first else {return}
-            
+
             guard let food = firstResult.labels.first else {return}
-            print("\(food.identifier) = \(food.confidence)")
+//            print("\(food.identifier) = \(food.confidence)")
             
+            self.updateIsShowValue(prediction: "\(food.identifier) = \(food.confidence)")
+
         }]
     }
-    
-    
+
+
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
 //        print("Camera was able to capture a frame", Date())
         setupVision()
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        
-        
+
+
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform(self.requests)
+    }
+    
+    
+    func updateIsShowValue(prediction: String){
+        customDelegate?.didUpdateWithValue(prediction)
     }
 }

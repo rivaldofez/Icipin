b6a1043f25b0087247b9fb49e4e8d1c0552bf4fb
rivaldofez <rivaldofez@gmail.com>
2022-05-21 11:57:41 +0700

    add simple dummy data

---
+++ McEat/DummyData/QuestData.swift
@@ -0,0 +1,23 @@
+//
+//  QuestData.swift
+//  McEat
+//
+//  Created by Rivaldo Fernandes on 21/05/22.
+//
+
+import SwiftUI
+
+class QuestData {
+    var questData = [QuestItem]()
+    
+    init(){
+        questData.append(contentsOf: [
+            QuestItem(title: "Santan Kental", image: "soto_betawi",
+                      color: [ Color(red: 25/255, green: 235/255, blue: 205/255),
+                               Color(red: 0/255, green: 146/255, blue: 109)
+                                ],
+                      isUnlock: false)
+            
+        ])
+    }
+}

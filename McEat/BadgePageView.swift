af8b8748c56b51432f40a23ed98891a87166789c
rivaldofez <rivaldofez@gmail.com>
2022-05-21 15:44:30 +0700

    add badge grid

--- McEat/BadgePageView.swift
+++ McEat/BadgePageView.swift
@@ -9,29 +9,13 @@ import SwiftUI
 
 
 struct BadgePageView: View {
+    var columnGrid = Array(repeating: GridItem(), count: 2)
+    
     var body: some View {
         GeometryReader { geo in
             
             VStack {
                 ZStack{
-                    HStack {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.white)
-                        VStack(alignment:.leading) {
-                            Text("Lokasi Kamu")
-                                .font(.system(.caption))
-                                .foregroundColor(.white)
-                            Text("Jakarta")
-                                .font(.system(.body).bold())
-                                .foregroundColor(.white)
-                        }
-                        Image(systemName: "chevron.down")
-                            .foregroundColor(.white)
-                        Spacer()
-                    }.onTapGesture {
-                        print("hello")
-                    }.padding(.leading, 20)
-                    
                 }
                 .frame(width: geo.size.width, height: 150)
                 .background(Corners(color: .red, tl: 0, tt: 0, bl: 80, bt: 80))
@@ -45,11 +29,25 @@ struct BadgePageView: View {
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .offset(x: 0, y: -40)
                 
+                
+                LazyVGrid(columns: columnGrid){
+                    ForEach((0...10), id: \.self){_ in
+                        ZStack{
+                            Image("soto_betawi")
+                                .resizable()
+                                .frame(width: (geo.size.width/2)-60, height: (geo.size.width/2)-60)
+                        }
+                        .frame(width: (geo.size.width/2)-40, height: (geo.size.width/2)-40)
+                        .background(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .shadow(color: .gray, radius: 3, x: 0, y: 0)
+                        .padding(20)
+                        
+                }
+            }
+                
             }.navigationBarHidden(true)
                 .edgesIgnoringSafeArea(.top)
-            
-            
-            
         }
     }
 }

f0707459752d2e6e55bac932dcb55a790025afca
rivaldofez <rivaldofez@gmail.com>
2022-05-22 09:29:13 +0700

    add struct custom color

---
+++ McEat/CustomView/CustomColor.swift
@@ -0,0 +1,15 @@
+//
+//  CustomColor.swift
+//  McEat
+//
+//  Created by Rivaldo Fernandes on 22/05/22.
+//
+
+import SwiftUI
+
+struct CustomColor {
+    static let primary = Color("primary")
+    static let secondary = Color("secondary")
+    static let gray = Color("gray")
+    static let white = Color("white")
+}

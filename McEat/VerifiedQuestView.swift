4a859092fd4434cac01a6e70d67a0f5006c7f996
rivaldofez <rivaldofez@gmail.com>
2022-05-22 08:49:50 +0700

    create blur view representable

--- McEat/VerifiedQuestView.swift
+++ McEat/VerifiedQuestView.swift
@@ -30,3 +30,19 @@ struct VerifiedQuestView_Previews: PreviewProvider {
         VerifiedQuestView()
     }
 }
+
+struct BlurView: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    
+    func makeUIView(context: Context) -> some UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+}

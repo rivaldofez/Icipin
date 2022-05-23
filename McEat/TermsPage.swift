8fc35b6e176686657cd6be03cf3ebae10bdb6e31
rivaldofez <rivaldofez@gmail.com>
2022-05-23 13:06:27 +0700

    create terms pages

--- McEat/TermsPage.swift
+++ McEat/TermsPage.swift
@@ -11,6 +11,42 @@ struct TermsPage: View {
     var body: some View {
         ZStack {
             CustomColor.white
+            VStack(alignment:.center){
+                Spacer()
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                
+                Text("ICIPIN")
+                    .font(.system(.title2).bold())
+                    .foregroundColor(.black)
+                
+                Text("Created by: McEat (Team 13 Afternoon Session")
+                    .font(.system(.body))
+                    .foregroundColor(.black)
+                    .padding(5)
+                
+                Spacer()
+                
+                Text("Credit For Non-Commercial Asset")
+                    .font(.system(.body).bold())
+                    .foregroundColor(.black)
+                    .padding(5)
+                Text("Icons made by max.icons from www.flaticon.com")
+                    .font(.system(.body))
+                    .foregroundColor(.black)
+                    .padding(5)
+                Text("Icons made by meds from www.flaticon.com")
+                    .font(.system(.body))
+                    .foregroundColor(.black)
+                    .padding(5)
+                Text("Icons by Icons8 https://icons8.com")
+                    .font(.system(.body))
+                    .foregroundColor(.black)
+                    .padding(5)
+                Spacer()
+            }
         }
         .navigationBarTitleDisplayMode(.inline)
         .navigationTitle("Terms and Conditional")

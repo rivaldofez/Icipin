0aa901e6ab5c429dbbb00b27f412bf35703d10cf
rivaldofez <rivaldofez@gmail.com>
2022-05-28 09:10:39 +0700

    update term & condition page

--- McEat/TermsPage.swift
+++ McEat/TermsPage.swift
@@ -13,38 +13,47 @@ struct TermsPage: View {
             CustomColor.white
             VStack(alignment:.center){
                 Spacer()
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                
-                Text("ICIPIN")
-                    .font(.system(.title2).bold())
-                    .foregroundColor(.black)
-                
-                Text("Created by: McEat (Team 13 Afternoon Session)")
-                    .font(.system(.body))
-                    .foregroundColor(.black)
-                    .padding(5)
+                Group {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    
+                    Text("ICIPIN")
+                        .font(.system(.title2).bold())
+                        .foregroundColor(.black)
+                    
+                    Text("Created by: McEat (Team 13 Afternoon Session)")
+                        .font(.system(.body))
+                        .foregroundColor(.black)
+                        .padding(5)
+                }
                 
                 Spacer()
                 
-                Text("Credit For Non-Commercial Asset")
-                    .font(.system(.body).bold())
-                    .foregroundColor(.black)
-                    .padding(5)
-                Text("Icons made by max.icons from www.flaticon.com")
-                    .font(.system(.body))
-                    .foregroundColor(.black)
-                    .padding(5)
-                Text("Icons made by meds from www.flaticon.com")
-                    .font(.system(.body))
-                    .foregroundColor(.black)
-                    .padding(5)
-                Text("Icons by Icons8 https://icons8.com")
-                    .font(.system(.body))
-                    .foregroundColor(.black)
-                    .padding(5)
+                Group{
+                    Text("Credit For Non-Commercial Asset")
+                        .font(.system(.body).bold())
+                        .foregroundColor(.black)
+                        .padding(5)
+                    Text("Icons made by max.icons from www.flaticon.com")
+                        .font(.system(.body))
+                        .foregroundColor(.black)
+                        .padding(5)
+                    Text("Icons made by meds from www.flaticon.com")
+                        .font(.system(.body))
+                        .foregroundColor(.black)
+                        .padding(5)
+                    Text("Icons by Icons8 https://icons8.com")
+                        .font(.system(.body))
+                        .foregroundColor(.black)
+                        .padding(5)
+                    Text("Image of food retrieve from various public website")
+                        .font(.system(.body))
+                        .foregroundColor(.black)
+                        .padding(5)
+                }
+                
                 Spacer()
             }
         }
